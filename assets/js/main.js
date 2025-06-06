7c0cad9dd57e503a0813da7a6698406b399ea725
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-06 14:25:21 +0200

    Aggiungi file HTML e JavaScript per il calcolo del prezzo del biglietto

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+/*Scrivere un programma che chieda all’utente:
+
+Il numero di chilometri da percorrere
+Età del passeggero Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+va applicato uno sconto del 20% per i minorenni
+
+va applicato uno sconto del 40% per gli over 65.
+
+*/
+
+//Il numero di chilometri da percorrere
+
+let choiseKmUser = Number(prompt("Quanti km vuoi percorrere?"));
+
+console.log(choiseKmUser);
+
+//Età del passeggero
+let ageUser = Number(prompt("Quanti anni hai?"));
+console.log(ageUser);
+
+//
