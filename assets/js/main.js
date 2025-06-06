25dc47c0cfa548f4d397760939da0c0138aabd28
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-06 14:41:21 +0200

    aggiunta funziona per calcore il prezzo del biglietto

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,4 +21,24 @@ console.log(choiseKmUser);
 let ageUser = Number(prompt("Quanti anni hai?"));
 console.log(ageUser);
 
-//
+//calocare il prezzo del biglietto
+
+function calcolaPrezzo(numeroKm, etauUser) {
+  const priceKm = 0.21;
+  const priceBiglietto = numeroKm * priceKm;
+  const scontoMinorenni = (priceBiglietto * 20) / 100;
+  const bliglietoMino = priceBiglietto - scontoMinorenni;
+  const scontoOver = (priceBiglietto * 40) / 100;
+  const bliglietoOver = priceBiglietto - scontoOver;
+  if (etauUser < 18) {
+    return bliglietoMino.toFixed(2);
+  } else if (etauUser > 65) {
+    return bliglietoOver.toFixed(2);
+  } else {
+    return `La tua èta non benifica di sconti e quindi il prezzp del biglietto è ${priceBiglietto.toFixed(
+      2
+    )}  `;
+  }
+}
+
+console.log(calcolaPrezzo(choiseKmUser, ageUser));
